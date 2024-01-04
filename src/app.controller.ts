213479f56726a1aede9207ced30eb2d59daaa8a1
jeelpatel231 <jeelpatel231@gmail.com>
2024-01-04 15:28:28 +0530

    get the user directly pass from middleware

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -4,6 +4,8 @@ import { CouchDbService } from 'src/common/services/Connection';
 import { JWTService } from 'src/common/services/JWTService';
 import { plainToInstance } from 'class-transformer';
 import { SavedUserDto } from 'lib/dto/User';
+import { CurrentUser } from './decorators/User.decorator';
+import { JwtUserPayload } from './common/middleware/User.middleware';
 
 @Controller()
 export class AppController {
@@ -12,26 +14,15 @@ export class AppController {
     private readonly jwtService: JWTService
   ) {}
 
-  // TODO: make an authentication middleware 
-  // to pass the current user around 
-  // all over the app
-
   @Get()
-  async getHello(@Cookies('jwt') jwtToken: string): Promise<string> {
+  async getHello(@CurrentUser() currentUser: JwtUserPayload): Promise<string> {
 
-    if(jwtToken == null) {
+    if(currentUser == null) {
       return "Hello World (not logged in)"
     }
 
-    const decodedPayload = await this.jwtService.decodeJwt(jwtToken)
-
-    if(typeof decodedPayload == 'string'){
-      return decodedPayload
-    }
-
-    const userBucket = await this.dbService.userBucket()
-    const userFromDb = await userBucket.defaultCollection()
-      .get(decodedPayload.email)
+    const userCollection = await this.dbService.getCollection("user")
+    const userFromDb = await userCollection.get(currentUser.email)
   
     const validated = plainToInstance(SavedUserDto, userFromDb.value)
 
