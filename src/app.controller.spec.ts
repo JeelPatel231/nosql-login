34c3aeaa032e7569d36818863d6c52ec75f36126
jeelpatel231 <jeelpatel231@gmail.com>
2024-01-05 16:09:53 +0530

    add correct dependencies for controller's test

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,5 +1,8 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
+import { CommonModule } from './common/common.module';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
 
 describe('AppController', () => {
   let appController: AppController;
@@ -8,14 +11,15 @@ describe('AppController', () => {
     const app: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
       providers: [],
+      imports: [CommonModule, AuthModule, UserModule],
     }).compile();
 
     appController = app.get<AppController>(AppController);
   });
 
-  // describe('root', () => {
-  //   it('should return "Hello World!"', () => {
-  //     expect(appController.getHello()).toBe('Hello World!');
-  //   });
-  // });
+  describe('root', () => {
+    it('should be defined"', () => {
+      expect(appController).toBeDefined()
+    });
+  });
 });
--- src/user/user.controller.spec.ts
+++ src/user/user.controller.spec.ts
@@ -1,12 +1,16 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserController } from './user.controller';
+import { CommonModule } from 'src/common/common.module';
+import { UserService } from './user.service';
 
 describe('UserController', () => {
   let controller: UserController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [CommonModule],
       controllers: [UserController],
+      providers: [UserService]
     }).compile();
 
     controller = module.get<UserController>(UserController);
