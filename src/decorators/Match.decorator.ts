0bbb424e6fcbbc44dea0abc7565d01b408c7237b
jeelpatel231 <jeelpatel231@gmail.com>
2024-01-08 13:58:36 +0530

    removed unused code

--- src/decorators/Match.decorator.ts
+++ src/decorators/Match.decorator.ts
@@ -1,28 +1,4 @@
-import {registerDecorator, ValidationArguments, ValidationOptions, ValidatorConstraint, ValidatorConstraintInterface} from 'class-validator';
-
-export function Match(property: string, validationOptions?: ValidationOptions) {
-    return (object: any, propertyName: string) => {
-        registerDecorator({
-            target: object.constructor,
-            propertyName,
-            options: validationOptions,
-            constraints: [property],
-            validator: MatchConstraint,
-        });
-    };
-}
-
-@ValidatorConstraint({name: 'Match'})
-export class MatchConstraint implements ValidatorConstraintInterface {
-
-    validate(value: any, args: ValidationArguments) {
-        const [relatedPropertyName] = args.constraints;
-        const relatedValue = (args.object as any)[relatedPropertyName];
-        return value === relatedValue;
-    }
-
-}
-
+import {registerDecorator, ValidationArguments, ValidationOptions } from 'class-validator';
 
 export function IsEqualTo(property: string, validationOptions?: ValidationOptions) {
     return (object: any, propertyName: string) => {
